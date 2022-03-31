75c7ae1b7b0b1697396de9168dccaf4985f64c2f
NunesNathan <nunesxd7@hotmail.com>
2022-03-31 10:17:22 -0300

    :construction: (model) - SalesModel structure

---
+++ models/salesModel.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const getAllSales = async () => { };
+
+const getSaleById = async () => { };
+
+module.exports = {
+  getAllSales,
+  getSaleById,
+};
