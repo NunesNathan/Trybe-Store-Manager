8fc056438bfdec44a03519c8b1b7499fe14c5b8f
NunesNathan <nunesxd7@hotmail.com>
2022-03-31 10:16:38 -0300

    :construction: (model) - ProductsModel structure

---
+++ models/productsModel.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const getAllProducts = async () => { };
+
+const getProductById = async () => { };
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
