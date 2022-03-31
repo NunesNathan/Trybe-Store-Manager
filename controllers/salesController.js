6b6a6920806be74e61550ccae3b17a85ed048a0b
NunesNathan <nunesxd7@hotmail.com>
2022-03-31 19:09:13 -0300

    :construction: (controller) - Sale getById

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,11 +1,19 @@
 const SalesModel = require('../models/salesModel');
+const salesServices = require('../services/salesService');
 
 const getAll = async (_req, res) => {
   const sales = await SalesModel.getAll();
   return res.status(200).json(sales);
 };
 
-const getById = async () => { };
+const getById = async (req, res) => {
+  const sale = await salesServices.getById(req.params);
+
+  if (!sale) {
+    return res.status(404).json({ message: 'Sale not found' });
+  }
+  return res.status(200).json(sale);
+};
 
 module.exports = {
   getAll,
