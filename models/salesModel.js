5865fe6acbb232e2f35125af9748c45c86b4e336
NunesNathan <nunesxd7@hotmail.com>
2022-03-31 15:48:33 -0300

    :construction: (model) - Order in queries

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,7 +1,8 @@
 const connection = require('./connection');
 
 const getAll = async () => {
-  const [sales] = await connection.execute('SELECT * FROM sales');
+  const [sales] = await connection
+    .execute('SELECT * FROM StoreManager.sales_products ORDER BY sale_id, product_id;');
   return sales;
 };
 
