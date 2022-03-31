869896fa17ed899216d0ea8d3135fb716a2f4c3d
NunesNathan <nunesxd7@hotmail.com>
2022-03-31 19:05:31 -0300

    :construction: (models) - Fix SalesModel queries

--- models/salesModel.js
+++ models/salesModel.js
@@ -2,11 +2,22 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const [sales] = await connection
-    .execute('SELECT * FROM StoreManager.sales_products ORDER BY sale_id, product_id;');
+    .execute(`SELECT SP.sale_id as saleId, S.date, SP.product_id as productId, SP.quantity
+ FROM StoreManager.sales_products as SP
+ JOIN StoreManager.products AS P ON SP.product_id = P.id
+ JOIN StoreManager.sales AS S ON SP.sale_id = S.id ORDER BY SP.sale_id, SP.product_id;`);
   return sales;
 };
 
-const getById = async () => { };
+const getById = async (id) => {
+  const [sale] = await connection
+    .execute(`SELECT S.date, SP.product_id as productId, SP.quantity
+ FROM StoreManager.sales_products as SP
+ JOIN StoreManager.products AS P ON SP.product_id = P.id
+ JOIN StoreManager.sales AS S ON SP.sale_id = S.id WHERE SP.sale_id = ?
+ ORDER BY SP.product_id;`, [id]);
+  return sale;
+};
 
 module.exports = {
   getAll,
