20eb4029113d239f18323633cc1c2c4d1da5468b
NunesNathan <nunesxd7@hotmail.com>
2022-04-01 17:02:47 -0300

    :construction: (service) - Connects to model to update a product

--- services/productsServices.js
+++ services/productsServices.js
@@ -1,11 +1,18 @@
 const productsModel = require('../models/productsModel');
 
 const verifyConflictName = async ({ name }) => {
-  const [result] = await productsModel.verifyConflictName(name);
+  const [result] = await productsModel.verifyAlreadyName(name);
 
   return !result;
 };
 
+const updateProduct = async (id, name, quantity) => {
+  await productsModel.updateProduct(name, quantity, id);
+
+  return ({ id, name, quantity });
+};
+
 module.exports = {
   verifyConflictName,
+  updateProduct,
 };
