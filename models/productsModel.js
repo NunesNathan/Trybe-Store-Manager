1f343694efb1fac3b238dc6e71231ef61d16e3c0
NunesNathan <nunesxd7@hotmail.com>
2022-04-01 01:49:19 -0300

    :construction: (model) - Verify name querie

--- models/productsModel.js
+++ models/productsModel.js
@@ -8,6 +8,7 @@ const getAll = async () => {
 const getById = async ({ id }) => {
   const [product] = await connection
     .execute('SELECT * FROM StoreManager.products WHERE id = ?;', [id]);
+
   return product;
 };
 
@@ -15,11 +16,20 @@ const postProduct = async ({ name, quantity }) => {
   const [product] = await connection
     .execute(`INSERT INTO StoreManager.products (name, quantity)
      VALUES (?, ?)`, [name, quantity]);
+
   return ({ id: product.insertId, name, quantity });
+};
+
+const verifyConflictName = async (name) => {
+  const [product] = await connection
+    .execute('SELECT * FROM StoreManager.products WHERE name = ?;', [name]);
+
+  return product;
 };
 
 module.exports = {
   getAll,
   getById,
   postProduct,
+  verifyConflictName,
 };
