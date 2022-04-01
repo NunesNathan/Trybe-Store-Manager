9bb4784147dd57cf60369c7463236bc6d25d8aad
NunesNathan <nunesxd7@hotmail.com>
2022-04-01 01:53:40 -0300

    :construction: (middleware) - 409 name exists

--- middlewares/productsMiddleware.js
+++ middlewares/productsMiddleware.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const productsServices = require('../services/productsServices');
 
 const productsFormatSchema = Joi.object({
   name: Joi.string().required(),
@@ -12,15 +13,25 @@ const productsValuesSchema = Joi.object({
 
 const validadeProduct = (req, res, next) => {
   const { error: format } = productsFormatSchema.validate(req.body);
-  console.log(format);
   if (format) return res.status(400).json({ message: format.message });
+
   const { error: values } = productsValuesSchema.validate(req.body);
-  console.log(values);
   if (values) return res.status(422).json({ message: values.message });
+
+  next();
+};
+
+const verifyConflictName = async (req, res, next) => {
+  const ableToInsert = await productsServices.verifyConflictName(req.body);
+
+  if (!ableToInsert) {
+    return res.status(409).json({ message: 'Product already exists' });
+  }
 
   next();
 };
 
 module.exports = {
   validadeProduct,
+  verifyConflictName,
 };
