1d9d4edabf13bc6463f4928fd130d393147c4f39
NunesNathan <nunesxd7@hotmail.com>
2022-04-02 01:01:42 -0300

    :construction: (middleware) - Remove array type & add for loop

--- middlewares/salesMiddleware.js
+++ middlewares/salesMiddleware.js
@@ -1,25 +1,25 @@
 const Joi = require('joi');
 
-const saleFormatSchema = Joi.array().items(
-  Joi.object({
+const saleFormatSchema = Joi.object({
     productId: Joi.number().required(),
     quantity: Joi.number().required(),
-  }),
-);
+});
 
-const saleValuesSchema = Joi.array().items(
-  Joi.object({
+const saleValuesSchema = Joi.object({
   productId: Joi.number().min(1),
   quantity: Joi.number().min(1),
-  }),
-);
+});
 
 const validadeSale = (req, res, next) => {
-  const { error: format } = saleFormatSchema.validate(req.body);
-  if (format) return res.status(400).json({ message: format.message });
+  for (let index = 0; index < req.body.length; index += 1) {
+    const saleToVerify = req.body[index];
 
-  const { error: values } = saleValuesSchema.validate(req.body);
-  if (values) return res.status(422).json({ message: values.message });
+    const { error: format } = saleFormatSchema.validate(saleToVerify);
+    if (format) return res.status(400).json({ message: format.message });
+    
+    const { error: values } = saleValuesSchema.validate(saleToVerify);
+    if (values) return res.status(422).json({ message: values.message });
+  }
 
   next();
 };
