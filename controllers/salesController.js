3e22759dcc8ba074acc0492933016f19e6227e68
NunesNathan <nunesxd7@hotmail.com>
2022-04-02 01:55:32 -0300

    :white_check_mark: (challenge) - Create putSale route callback

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -23,6 +23,12 @@ const insertSale = async (req, res) => {
   return res.status(201).json(result);
 };
 
+const updateSale = async (req, res) => {
+  const result = await salesServices.updateSale(req.params, req.body);
+
+  res.status(200).json(result);
+};
+
 module.exports = {
   getAll,
   getById,
@@ -32,6 +38,6 @@ module.exports = {
   ],
   putSale: [
     salesMiddleware.validadeSale,
-    // ,
+    updateSale,
   ],
 };
