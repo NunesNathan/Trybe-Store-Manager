04c0e3abe8dddaa20db17f568753e2aedda68495
NunesNathan <nunesxd7@hotmail.com>
2022-04-01 23:06:23 -0300

    :construction: (middleware) - Prepare verification to array

--- middlewares/salesMiddleware.js
+++ middlewares/salesMiddleware.js
@@ -1,22 +1,24 @@
 const Joi = require('joi');
 
-const saleFormatSchema = Joi.object({
-  productId: Joi.number().required(),
-  quantity: Joi.number().required(),
-});
+const saleFormatSchema = Joi.array().items(
+  Joi.object({
+    productId: Joi.number().required(),
+    quantity: Joi.number().required(),
+  }),
+);
 
-const saleValuesSchema = Joi.object({
-  productId: Joi.number().min(5),
+const saleValuesSchema = Joi.array().items(
+  Joi.object({
+  productId: Joi.number().min(1),
   quantity: Joi.number().min(1),
-});
+  }),
+);
 
 const validadeSale = (req, res, next) => {
   const { error: format } = saleFormatSchema.validate(req.body);
-  console.log(format);
   if (format) return res.status(400).json({ message: format.message });
 
   const { error: values } = saleValuesSchema.validate(req.body);
-  console.log(values);
   if (values) return res.status(422).json({ message: values.message });
 
   next();
