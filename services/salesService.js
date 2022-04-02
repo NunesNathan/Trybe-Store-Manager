347e345398569d18cbc23767901133dea539b1f8
NunesNathan <nunesxd7@hotmail.com>
2022-04-01 23:04:10 -0300

    :construction: (service) - Return create sale logic

--- services/salesService.js
+++ services/salesService.js
@@ -1,13 +1,25 @@
-const SalesModel = require('../models/salesModel');
+const salesModel = require('../models/salesModel');
 
 const getById = async ({ id }) => {
-  const sale = await SalesModel.getById(id);
+  const sale = await salesModel.getById(id);
   if (!sale[0]) {
     return false;
   }
   return sale;
 };
 
+const postSale = async (body) => {
+  const { insertId: id } = await salesModel.createSale();
+
+  const result = await Promise.all(
+    body.map(async ({ productId, quantity }) => (
+      salesModel.postSale(id, productId, quantity))),
+  );
+
+  return ({ id, itemsSold: result });
+};
+
 module.exports = {
   getById,
+  postSale,
 };
