9873136e124d757446532ceb5095ef7ed78b933e
NunesNathan <nunesxd7@hotmail.com>
2022-04-04 19:18:00 -0300

    :stethoscope: (test) - getById products

--- test/unit/models/productsModels.js
+++ test/unit/models/productsModels.js
@@ -21,10 +21,32 @@ describe('productsModels', () => {
       expect(response).to.be.equals(mocked.getProducts)
     });
     
-    it('get all products returns with wrong type', async () => {
+    it('get all products returns without array', async () => {
       const response = await productsModel.getAll();
 
       expect(response).not.to.be.equals([mocked.getProducts])
+    });
+  });
+
+  describe('get product by id', async () => {
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([[mocked.getProductById]])
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('get one product', async () => {
+      const response = await productsModel.getById();
+
+      expect(response).to.be.equals(mocked.getProductById)
+    });
+    
+    it('get one product without array', async () => {
+      const response = await productsModel.getById();
+
+      expect(response).not.to.be.equals([mocked.getProductById])
     });
   });
 });
